937fa0e2f24143583092a95623a9de57b15c29fa
DreDev3 <andreedson02@gmail.com>
2024-04-27 15:55:53 -0300

    update model.css and script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let count = 1
-let count2 = 2
+let count2 = 1
 
 document.getElementById('radio1').checked = true
 
@@ -19,13 +19,13 @@ function nextImage() {
 document.getElementById('radioM1').checked = true
 
 setInterval ( function () {
-    nextImage()
+    nextImage2()
 }, 4000)
 
-function nextImage() {
-    count++
-    if( count > 4) {
-        count = 1
+function nextImage2() {
+    count2++
+    if( count2 > 4) {
+        count2 = 1
     }
 
     document.getElementById('radioM'+count).checked = true
